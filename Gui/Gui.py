57c2c0dcfd6cbfb193f150ea517d0c3bbf7c767f
JackUlbrichBaker <jackubaker@gmail.com>
2024-01-19 08:06:13 +1100

    Initial commit

---
+++ Gui/Gui.py
@@ -0,0 +1,355 @@
+
+from customtkinter import *
+from tkinter import *
+from tkinter import scrolledtext
+import serial
+import time
+from datetime import datetime
+from math import log, ceil
+
+# from STIM300 data sheet pg38, table5-21
+# x32 + x26 + x23 + x22 + x16 + x12 + x11 + x10 + x8 + x7 + x5 + x4 + x2 + x + 1
+
+STIM300_divisor = 0b100000100110000010001110110110111
+
+#def CRC_Check(data, divisor = STIM300_divisor):
+
+class base_frame(CTkFrame):
+    def __init(self, master):
+        self.row = 0
+        self.column = 0
+        self.master = CTkFrame()
+
+    def update_grid(self, row=2, column=0, rowspan=1,columnspan=1,sticky="nsew",padx=20,pady=10):
+        self.row = row
+        self.column = column
+        self.grid(row = self.row, column = self.column, rowspan=rowspan, columnspan=columnspan, sticky=sticky, padx=padx, pady=pady)
+    
+    # class function to change the serial port
+    def change_serial_port(self, new_port):
+            self.serial_port.close()
+            self.serial_port = self.root.serial_init(new_port)
+
+    def listen_serial(self):
+            bytes = 4
+            listened = self.root.serial_listen.read(bytes)
+            print(listened)
+
+def CRC(dataword, generator):
+
+    dword = dataword
+    #find the length of the binary number in
+    l_gen = generator.bit_length()
+ 
+    # append 0s to dividend
+    dividend = dword << (l_gen - 1)
+ 
+    # shft specifies the no. of least significant
+    # bits not being XORed
+    shft = ceil(log(dividend + 1, 2)) - l_gen
+ 
+    # ceil(log(dividend+1 , 2)) is the no. of binary
+    # digits in dividend
+ 
+    while dividend >= generator or shft >= 0:
+ 
+        # bitwise XOR the MSBs of dividend with generator
+        # replace the operated MSBs from the dividend with
+        # remainder generated
+        rem = (dividend >> shft) ^ generator
+        dividend = (dividend & ((1 << shft) - 1)) | (rem << shft)
+ 
+        # change shft variable
+        shft = ceil(log(dividend+1, 2)) - l_gen
+ 
+    # finally, AND the initial dividend with the remainder (=dividend)
+    codeword = dword << (l_gen-1) | dividend
+    print("Remainder:", bin(dividend))
+    print("Codeword :", bin(codeword))
+
+CRC(0b10011101, 0b1001)
+
+
+#################################
+#  Dark mode is the best mode   #
+#################################
+set_appearance_mode("dark")#
+#################################
+
+class port_droplist(CTkOptionMenu):
+    def __init__(self, master):
+        CTkOptionMenu.__init__(self, master=master)
+
+        #inherit the root app from master
+        self.root = master.root
+        #inherit the serial port from root
+        self.serial_in = self.root.serial_in
+
+        self.chosen_option_in = StringVar()
+        self.chosen_option_out = StringVar()
+
+
+        self.checklist_in = CTkOptionMenu(master=master, values=self.get_ports(), command=lambda _: self.change_serial_in(new_port= int(self.chosen_option_in.get())), variable = self.chosen_option_in)
+        self.checklist_in.grid(row=1, column=0, sticky="n", pady = [0, 5])
+
+        self.checklist_out = CTkOptionMenu(master=master, values=self.get_ports(), command=lambda _: self.change_serial_out(new_port= int(self.chosen_option_out.get())), variable = self.chosen_option_out)
+        self.checklist_out.grid(row=2, column=0, sticky="n", pady = [0, 5])
+
+    def change_serial_in(self, new_port):
+        self.serial_in = self.root.change_serial_in(new_port = new_port)
+    
+    def change_serial_out(self, new_port):
+        self.serial_out = self.root.change_serial_out(new_port = new_port)
+    
+    def get_ports(self):
+        availiable_ports = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+        availiable_ports = [str(element) for element in availiable_ports]
+
+        #########################################################
+        # properly impliment when you have time                 #
+        #########################################################
+        return availiable_ports
+    
+    def update_grid(self, row=2, column=0):
+
+        self.row = row
+        self.column = column
+        #self.checklist_in.grid(row=self.row, column=self.column, padx=0, pady=0)
+        #self.checklist_out.grid(row=self.row+1, column=self.column, padx=0, pady=0)
+
+        self.grid(row = self.row, column = self.column)
+    
+
+
+class send_frame(base_frame):
+    def __init__(self, master):
+        super(send_frame, self).__init__(master)
+        
+        # CTK Var to store user input
+        self.text_in = ""
+        self.root = master
+        self.serial_out = self.root.serial_out
+
+
+        #add buttons to trigger send event
+        self.button = CTkButton(self, text = "Send Normal Mode Datagram", command = lambda: self.send_serial("N\r"))
+        self.button.grid(row=0, column=0, sticky="ew", padx=20, pady=10)
+
+        #add buttons to trigger send event
+        self.button = CTkButton(self, text = "Send Serial Datagram", command = lambda: self.send_serial("I\r"))
+        self.button.grid(row=1, column=0, sticky="ew", padx=20, pady=[0,10])
+        
+        #add label for buttons
+        self.grid_columnconfigure(0, weight=1)
+        self.label = CTkLabel(self, text="Press button to send custom command")
+        self.label.grid(row=2, column=0, sticky="NS")
+
+        #add entry for user input to be received.
+        self.input = CTkEntry(self, textvariable=self.text_in, placeholder_text = "eg: 'N'")
+        self.input.grid(row=3, column=0, padx=20, pady=10)
+        #add buttons to trigger send event
+        self.button = CTkButton(self, text = "Send Serial Datagram", command = self.send_entry)
+        self.button.grid(row=4, column=0, sticky="ew", padx=20, pady=[0,10])
+
+    def send_serial(self, char):
+        self.serial_out.write(str.encode(char))
+
+    def send_entry(self):
+        #update variable and send via serial
+        self.text_in = self.input.get()
+        self.serial_out.write(str.encode(self.text_in))
+        print(self.text_in)
+
+
+
+class read_frame(base_frame):
+    def __init__(self, master):
+        super(read_frame, self).__init__(master)
+
+        self.root = master
+        self.serial_in = self.root.serial_in
+        
+        self.row = 2
+        self.column = 0
+
+        
+        self.label = CTkLabel(self, text="INCOMING TEXT")
+        self.label.grid(row=0, column=0, padx=2, pady=2,sticky="ew")
+
+        self.serial_text = scrolledtext.ScrolledText(self, state="disabled")
+        self.serial_text.grid(row=1,column=0,sticky="ew")
+        
+
+        self.update_grid(row=2, column=1)
+
+        self.serial_scan()
+
+    # class function to change the serial port
+    def change_serial_port(self, new_port):
+        self.serial_in.close()
+        self.serial_in = self.root.change_serial_in(new_port)
+
+    def serial_scan(self):
+        self.serial_in = self.root.serial_in
+        serial_in = self.serial_in.read(64)
+        temp = int.from_bytes(serial_in, byteorder="big")
+        if(temp != 0):
+            self.update_text(bin(temp)+"\n")
+
+        
+
+        self.after(101, lambda: self.serial_scan())
+
+    # update the text in the frame
+    def update_text(self, text):
+        self.serial_text.configure(state="normal")
+        self.serial_text.insert(INSERT, text)
+        self.serial_text.configure(state="disabled")
+        
+        ##Change from polling when have time##
+        ## Might not be polling?  the pyserial library uses the system select func ??
+
+class port_label(base_frame):
+    def __init__(self, master):
+        super(port_label, self).__init__(master)
+
+        self.root = master.root
+        self.serial = self.root.serial_in
+        self.serial_out = self.root.serial_out
+        self.text = "Currently sending data to port: " + self.serial.name
+
+        self.column = 0
+        self.row = 6
+
+        self.label = CTkLabel(master=self, text = self.text)
+        self.label.grid(row = self.row, column = self.column, padx=0,pady=[10,5], sticky="s")
+        self.update_grid(row=1, column=1)
+
+        self.update_text()
+
+
+    # recursive function that updates itself every second to display the current serial port being used
+    def update_text(self):
+        self.serial = self.root.serial_in
+        self.serial_out = self.root.serial_out
+
+        temp_str = str(self.serial.name)[-1]
+        self.text = "  Currently receiving from port: " + temp_str + " \n " + "Currently sending to port: " + str(self.serial_out.name)[-1]
+        self.label.configure(text=self.text)
+        self.master.after(1000, self.update_text)
+
+
+class MainFrame(base_frame):
+    def __init__(self, master):
+        super(MainFrame, self).__init__(master)
+
+        self.root = master
+
+        self.row = 0
+        self.column = 0
+
+
+        # add widgets onto the frame
+        self.label = CTkLabel(master=self, text="  Please select the output port below: ")
+        self.label.grid(row=0, column=0)
+
+        self.checklist = port_droplist(self)
+        self.checklist.update_grid(row=1, column=0)
+
+        self.port_label = port_label(self)
+        self.port_label.update_grid(row=2, column=0)
+        
+        #self.button = CTkButton(self, text = "listen", command = lambda: listen_serial(serial_listen, bytes = 4))
+        #self.button.grid(row=1, column=0)
+
+
+        self.columnspan = 3
+        self.rowspan = 0
+
+        self.update_grid(0, 0)
+
+
+    def set_text(self, text):
+        self.label.configure(text=text)
+
+
+class App(CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.serial_in = self.serial_init(0)
+        self.serial_out = self.serial_init(1)
+
+        #Give the Gui a title
+        self.title("STIM300 Serial Interface")
+        #set windows dimensions
+        self.geometry("900x600")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=5)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+
+
+        #add in a frame
+        self.my_frame = MainFrame(master=self)
+        self.my_frame.update_grid(row = 0, column = 0)
+
+
+        self.read_frame = read_frame(self)
+        self.read_frame.update_grid(row = 0, column = 1)
+
+        self.send_frame = send_frame(self)
+        self.send_frame.update_grid(row=2, column=0)
+
+
+    def serial_init(self, port_number = 0, baud = 115200, timeout = 0):
+        serial_port = serial.Serial('/dev/pts/'+ str(port_number), baudrate=baud, 
+        timeout=timeout, parity = serial.PARITY_NONE, bytesize = serial.EIGHTBITS)
+        serial_port.rtscts = False
+        serial_port.dsrdtr = False
+
+        return serial_port
+    
+    def change_serial_in(self, new_port, baud = 115200):
+        if type(new_port) != int:
+            print("please input an int")
+            return 0
+        if new_port < 0 | new_port > 99:
+            print("invalid port number :(")
+            return 0
+            
+        self.serial_in.close()
+        temp = self.serial_init(port_number=new_port, baud=baud)
+        if temp.isOpen():
+            print("Now communitcating with port number: " + str(new_port) + "\n")
+            self.serial_in = temp
+            return self.serial_in
+
+        else:
+            print("Invalid Port")
+
+
+    def change_serial_out(self, new_port, baud = 115200):
+        if type(new_port) != int:
+            print("please input an int")
+            return 0
+        if new_port < 0 | new_port > 99:
+            print("invalid port number :(")
+            return 0
+            
+        self.serial_out.close()
+        temp = self.serial_init(port_number=new_port, baud=baud)
+        if temp.isOpen():
+            print("Now communitcating with port number: " + str(new_port) + "\n")
+            self.serial_out = temp
+            return self.serial_out
+
+        else:
+            print("Invalid Port")
+
+
+
+
+app = App()
+app.mainloop()
---
+++ data_stub/data_stub.cpp
@@ -0,0 +1,260 @@
+#include <stdio.h>
+#include <termios.h>  /* Adding POSIX style terminal control*/
+#include <unistd.h>   /* Standard UNIX definitions*/
+#include <fcntl.h>    /* File control */
+#include <errno.h>
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <thread>
+#include <iostream> 
+#include <fstream> 
+#include <cstdlib> 
+#include <bitset>
+
+
+using namespace std;
+
+/*  Initialize the serial port */
+int serial_init(int port_no) {
+
+
+    int serial_port = 0;
+    char* device_name = new char[100];
+
+    sprintf(device_name, "/dev/pts/%d", port_no);
+
+    serial_port = open(device_name, O_RDWR);
+
+    if (serial_port < 0) { 
+        std::cout <<  "Error opening port :(( \nError No: " << errno << endl;
+        return 0;
+    } 
+    else{
+        std::cout << "Yay!  Opened port! \n";
+    }
+    int speed = B1152000;
+    struct termios SerialPortSettings;
+    tcgetattr(serial_port, &SerialPortSettings);
+    cfsetispeed(&SerialPortSettings, 1152000);
+    cfsetospeed(&SerialPortSettings, 1152000);
+    SerialPortSettings.c_cflag &= ~PARENB;
+    SerialPortSettings.c_cflag &= ~CSTOPB;
+    SerialPortSettings.c_cflag &= ~CSIZE;
+    SerialPortSettings.c_cflag |= CS8;
+    SerialPortSettings.c_cflag &= ~CRTSCTS;
+    SerialPortSettings.c_cflag |= CREAD | CLOCAL;
+    SerialPortSettings.c_iflag &= ~(IXON |IXOFF|IXANY);
+    SerialPortSettings.c_iflag &= ~(ICANON |ECHO | ECHOE | ISIG);
+    SerialPortSettings.c_oflag &= ~OPOST;
+    SerialPortSettings.c_cc[VMIN] = 1;
+    SerialPortSettings.c_cc[VTIME] = 0;
+
+    //  CHECK AGAINST THE DATASHEET AND RS232 SPEC
+
+    if((tcsetattr(serial_port, TCSANOW, &SerialPortSettings)) != 0){
+        printf("\n Error in setting Attributes!!!  Please check my code because something has gone terribly wrong :(( \n");
+    }
+    else {
+        std:cout << "Baud rate is: " << cfgetispeed(&SerialPortSettings) << endl;
+    } 
+
+    return serial_port;
+}
+
+// simple func to send serial data easily
+int send_serial(char* msg, int serial_port){
+    int size = strlen(msg);
+        write(serial_port, msg, size);
+    return 0;
+}
+
+void start_ports(){
+    system("socat -d -d pty,raw,echo=0 pty,raw,echo=0");
+}
+
+char dummy_byte(void){
+    // Function to generate 3 bytes of dummy data (24bits)
+    char byte = rand()%256;
+    return byte;
+}   
+
+vector<int> find_ports(){
+    char* device_name = new char[100];
+    vector<int> valid_ports;
+
+    for (int i=1;i<99;i++) 
+        {
+        // Prepare the port name (Linux)
+
+        #ifdef __linux__
+            sprintf(device_name, "/dev/pts/%d", i);
+        #endif
+
+        // try to connect to the device
+        int port;
+        port = open(device_name, O_RDWR);
+
+        if (port >= 0)    
+            {
+                valid_ports.push_back(i);
+                close(port);
+            }
+        }
+    return valid_ports;
+}
+
+
+/*  This function creates the "normal mode datagram as per the STIM300 datasheet section 5.5.6"*/
+void normal_mode_datagram(int serial_port, bool startup = false, bool dummy_data = true){
+
+
+    //int termination = 78;  //  This is byte 1 of the serial number datagram if there IS CR+TF termination
+    char identifier = 0b10101111; // datagram Identifier for full datagram (64 bytes     )
+    char STATUS = 0b00000000;
+
+    /*  sorry this is a bit opaque but it sets the startup bit (bit 6) as per table 5-23*/
+    if (startup) {
+        STATUS = STATUS | 0b01000000; //  this uses the bitwise "OR" to set the startup bit to one
+    }
+    else{
+        STATUS = STATUS & 0b10111111; // uses bitwise "AND" to set the startup bit to zero
+    }
+
+
+    int datagram_length = 64;
+    char datagram[datagram_length];
+    char CR = 0b00001101;
+    memset(datagram, 0, datagram_length);
+
+    datagram[0] = identifier;
+    if (dummy_data){
+    #pragma unroll
+    for (int i = 1; i < 10; i++){
+        datagram[i] = dummy_byte();
+    } 
+    #pragma unroll
+    for (int i = 11; i < 20; i++){
+        datagram[i] = dummy_byte();
+    } 
+    #pragma unroll
+    for (int i = 21; i < 30; i++){
+        datagram[i] = dummy_byte();
+    } 
+    #pragma unroll
+    for (int i = 11; i < 37; i++){
+        datagram[i] = dummy_byte();
+    } 
+    #pragma unroll
+    for (int i = 38; i < 44; i++){
+        datagram[i] = dummy_byte();
+    } 
+    #pragma unroll
+    for (int i = 45; i < 51; i++){
+        datagram[i] = dummy_byte();
+    } 
+    #pragma unroll
+    for (int i = 51; i < 55; i++){
+        datagram[i] = dummy_byte();
+    } 
+    }
+    //cout << "Status Byte: " << bitset<8>(STATUS) << endl;
+    datagram[10] = STATUS;
+    datagram[20] = STATUS;
+    datagram[30] = STATUS;
+    datagram[37] = STATUS;
+    datagram[44] = STATUS;
+    datagram[51] = STATUS;
+    datagram[55] = STATUS;
+    datagram[63] = CR;
+    //cout << datagram[0] << endl;
+
+    for (int i = 0; i < datagram_length; i++){
+        //cout << bitset<8>(datagram[i]) << endl;
+        send_serial(&datagram[i], serial_port);
+    }
+}
+
+void auto_normal_mode_datagram(int sleep_seconds, int serial_port, bool startup = false, bool dummy_data = true){
+    normal_mode_datagram(serial_port, startup, dummy_data);
+    sleep(sleep_seconds);
+    auto_normal_mode_datagram(sleep_seconds, serial_port, dummy_data);
+}
+
+int poll_serial_in(int serial_port_in, int serial_port_out){
+    string last_char = " ";
+
+
+    char read_buff[128];
+    memset(read_buff, 0, sizeof(read_buff));
+
+    while(1){
+        
+    int n = read(serial_port_in, &read_buff, 1);
+
+    if(n<0){
+        cout<<"\nERROR!\n";
+    }
+
+
+    if (last_char == "N" & read_buff[0] == 0b00001101){
+        cout << "HEy" << endl;
+        normal_mode_datagram(serial_port_out, false, true);
+
+    }
+    if(last_char != to_string(read_buff[0])){
+        last_char = read_buff[0];
+    }
+    //poll_serial_in(serial_port_in);
+    }
+}
+
+
+int main(void){
+    // initialize the random number generator with time to give pseudorand ints
+    srand(static_cast<unsigned int> (time(0)));
+    
+
+    std::thread t1(start_ports);
+
+    int serial_port = serial_init(1);
+
+
+
+    vector<int> ports = find_ports();
+    cout << to_string(ports.size());
+    for (int i = 0; i < ports.size(); i++){
+        printf("Availiable ports: %d \n", ports[i]);
+    }
+
+    float frequency = 1;
+    int port_num = 1;
+
+    cout << "What serial port do you want to communicate on?" << endl;
+    cin >> port_num;
+    
+    int serial_port_out = serial_init(port_num);
+
+    cout << "Please input the Frequency you want to send the normal mode datagram at: " << endl;    
+    cin >> frequency;
+    cout << "You have selected: " << frequency << "Hz" << endl;
+    float temp = 1/frequency;
+    
+    int sleep_seconds = (int)temp;
+    cout << "Sending datagram every " << sleep_seconds << " Seconds" << endl;
+    
+    
+    
+
+    std::thread auto_thread(auto_normal_mode_datagram, sleep_seconds, serial_port_out, true, true);
+    usleep(500);
+
+    int pp;
+    cout << "What serial in?" << endl;
+    cin >> pp;
+
+    int serial_port_in = serial_init(pp);
+    poll_serial_in(serial_port_in, serial_port_out);
+
+    while(1){}
+}
